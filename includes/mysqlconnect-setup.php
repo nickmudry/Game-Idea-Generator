8019827729645832923a9b94d51ad8b09fea8f41
Nick Mudry <nick@mudry.me>
2016-12-29 10:26:27 -0500

    Added a setup MySQL Connect File
    For future GitHub community contributions, but for now, just so
    something dooooes exist here... haha

---
+++ includes/mysqlconnect-setup.php
@@ -0,0 +1,18 @@
+<?php
+
+//Hi GitHub community! If you're installing this to your own server, here's the MySQL Connect file that's called. Fill in your database information below.
+
+//On the ToDo list is to make an installer that puts the appropiate tables in, and could be something the communtiy does if they have time for it, otherwise, it'll be done eventually... 
+
+//Save this as mysqlconnect.php in the /includes/ folder. 
+
+//Thanks! 
+
+//Connect to the mysql server
+//mysql_connect("SERVER", "USER", "PASS") or die(mysql_error());
+//echo "Connected to MySQL<br />";
+
+//Select your database! 
+//mysql_select_db("DATABASE") or die(mysql_error());
+//echo "Connected to Database";
+?>
